4960218dfcd450ee8ffb1e40e62b5f563b21e011
salaheddineazarg <salahazarag17@gmail.com>
2024-02-01 10:01:07 +0100

    Front-end page

--- src/main/java/com/example/youreviews/security/AppSecurityConfig.java
+++ src/main/java/com/example/youreviews/security/AppSecurityConfig.java
@@ -1,2 +1,17 @@
-package com.example.youreviews.security;public class AppSecurityConfig {
+package com.example.youreviews.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AppSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll()).build();
+    }
 }
